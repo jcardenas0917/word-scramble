cc7d0929cce661aac602c789ac8e3f03265861ba
jaime cardenas <jcardenas@fixcityrepairs.com>
2020-10-17 21:00:34 -0400

    scramble logic

--- src/app/components/game/game.component.ts
+++ src/app/components/game/game.component.ts
@@ -1,4 +1,10 @@
-import { Component, OnInit } from '@angular/core';
+import { WordsService } from './../../services/words.service';
+import { Component, OnInit, ViewChild } from '@angular/core';
+import { FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
+import { BehaviorSubject } from 'rxjs';
+
+const tryAgain = 'Sorry try again';
+const correct = 'You are correct';
 
 @Component({
   selector: 'app-game',
@@ -6,10 +12,73 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./game.component.scss']
 })
 export class GameComponent implements OnInit {
+  isCorrect$ = new BehaviorSubject<string>('');
+  message$ = new BehaviorSubject<string>('');
+  numberOfTries = 0;
+  numberOfWins = 0;
+  randonWord: string;
+  scrambledWord: string;
+  gameForm: FormGroup;
 
-  constructor() { }
-
-  ngOnInit() {
+  get playerWord(): FormControl {
+    return this.gameForm.get('playerWord') as FormControl;
   }
 
+
+  constructor(private wordService: WordsService) { }
+
+  ngOnInit() {
+    this.gameForm = new FormGroup({
+      playerWord: new FormControl(''),
+    });
+
+    this.wordService.getWords().subscribe(randomWord => this.randonWord = randomWord);
+    this.scrambleWord();
+  }
+
+  checkGuess() {
+    if (this.playerWord.value === this.randonWord) {
+      this.playerWord.value('')
+      this.isCorrect$.next('yes');
+      this.message$.next(correct);
+      this.numberOfWins = this.numberOfWins + 1;
+      this.wordService.getWords().subscribe(randomWord => this.randonWord = randomWord);
+      this.scrambleWord();
+    } else {
+      this.isCorrect$.next('no');
+      this.numberOfTries = this.numberOfTries + 1;
+      this.message$.next(tryAgain);
+      this.tryAgain();
+    }
+  }
+  private scrambleWord() {
+    this.scrambledWord = this.randonWord.split('').sort(() => 0.5 - Math.random()).join('');
+    if (this.randonWord === this.scrambledWord) {
+      this.scrambleWord();
+    }
+  }
+  gameOver() {
+    this.numberOfTries = 0;
+    this.playerWord.setValue(this.randonWord);
+    this.message$.next('Thank You For Playing!');
+    this.isCorrect$.next('game-over');
+  }
+
+  playAgain() {
+    this.message$.next('');
+    this.numberOfTries = 0;
+    this.numberOfWins = 0;
+    this.scrambleWord();
+    this.wordService.getWords().subscribe(randomWord => this.randonWord = randomWord);
+    this.scrambleWord();
+  }
+
+  clearField() {
+    this.playerWord.setValue('');
+    this.message$.next('');
+  }
+
+  private tryAgain() {
+    this.playerWord.setValue('');
+  }
 }
